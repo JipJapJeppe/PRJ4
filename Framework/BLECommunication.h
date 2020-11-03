dbb582313dd546865fa61986eeab1a9a31c5caee
JipJapJeppe <55808892+JipJapJeppe@users.noreply.github.com>
2020-11-03 10:47:08 +0100

    Framework
    Framework added - alle .c og .h filer

---
+++ Framework/BLECommunication.h
@@ -0,0 +1,24 @@
+/* ========================================
+ *
+ * Copyright YOUR COMPANY, THE YEAR
+ * All Rights Reserved
+ * UNPUBLISHED, LICENSED SOFTWARE.
+ *
+ * CONFIDENTIAL AND PROPRIETARY INFORMATION
+ * WHICH IS THE PROPERTY OF your company.
+ *
+ * ========================================
+*/
+
+/* [] END OF FILE */
+#pragma once
+#include "project.h"
+
+typedef struct BLE_Comm
+{
+    
+}BLE_Comm;
+
+void initBLE();
+void sendData(uint16 data);
+void connectBLE();
